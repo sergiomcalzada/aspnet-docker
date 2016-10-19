35dd1fd82ddd09471f03dbbe7bc89d07bd2d5128
Troy Dai <troy.dai@outlook.com>
2016-10-19 11:09:43 -0700

    update test

--- 1.1.0/jessie/tests/test.sh
+++ 1.1.0/jessie/tests/test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
-# test targets
-prod=aspnetcorenightly-exp.azurecer.io/aspnetcore:1.1.0-preview1-final
-build=aspnetcorenightly-exp.azurecer.io/aspnetcore-build:1.1.0-preview1-final
+root=$(cd $(dirname $0); pwd)
+pushd $root
 
 # test parameters
 timestamp=$(date "+%H%M%S")
+prod=aspnetcore:$timestamp
+build=aspnetcore-build:$timestamp
+
+# build images
+docker build -t $prod ../product/
+docker build -t $build ../build/
 
 # build sample app from sdk
 docker run --name build_sample_$timestamp --rm=true \
            -v $(cd ./appsrc; pwd):/appsrc -w /appsrc \
            $build ./build.sh
-
-# verify output
 
 # run sample in production image
 docker run --name prod_sample_$timestamp \
@@ -22,4 +24,12 @@ docker run --name prod_sample_$timestamp \
            -d \
            $prod dotnet ./appsrc.dll
 
+# sleep for 5 seconds
+sleep 5
+
+# test the web application
 curl http://localhost:5001
+
+docker rm -f prod_sample_$timestamp
+
+popd
